c588c023baca9df935baaca25d6c6210f4547566
anpospisil <anpospisil89@gmail.com>
2020-05-18 11:23:56 +0200

    new app

--- src/App.js
+++ src/App.js
@@ -7,16 +7,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>
+          Edit <code>src/Ah1p.js</code> and save to reload.
+        </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello, Bitch
         </a>
       </header>
     </div>
